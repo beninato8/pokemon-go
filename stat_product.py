47ada3276904d654f23ca9e9843a540607c14793
beninato8 <beninato8@gmail.com>
2019-11-21 03:11:36 -0600

    pprinting

--- stat_product.py
+++ stat_product.py
@@ -2,6 +2,7 @@ import json
 import pandas as pd
 import math
 import operator
+from pprint import pprint
 
 ALL_NAMES = "Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Alolan Rattata", "Rattata", "Alolan Raticate", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Alolan Raichu", "Raichu", "Alolan Sandshrew", "Sandshrew", "Alolan Sandslash", "Sandslash", "Nidoran", "Nidorina", "Nidoqueen", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Alolan Vulpix", "Vulpix", "Alolan Ninetales", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth", "Alolan Diglett", "Diglett", "Alolan Dugtrio", "Dugtrio", "Alolan Meowth", "Meowth", "Alolan Persian", "Persian", "Psyduck", "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop", "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel", "Alolan Geodude", "Geodude", "Alolan Graveler", "Graveler", "Alolan Golem", "Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetchd", "Doduo", "Dodrio", "Seel", "Dewgong", "Alolan Grimer", "Grimer", "Alolan Muk", "Muk", "Shellder", "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee", "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Alolan Exeggutor", "Exeggutor", "Cubone", "Alolan Marowak", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu", "Starmie", "Mr Mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo", "Mew", "Chikorita", "Bayleef", "Meganium", "Cyndaquil", "Quilava", "Typhlosion", "Totodile", "Croconaw", "Feraligatr", "Sentret", "Furret", "Hoothoot", "Noctowl", "Ledyba", "Ledian", "Spinarak", "Ariados", "Crobat", "Chinchou", "Lanturn", "Pichu", "Cleffa", "Igglybuff", "Togepi", "Togetic", "Natu", "Xatu", "Mareep", "Flaaffy", "Ampharos", "Bellossom", "Marill", "Azumarill", "Sudowoodo", "Politoed", "Hoppip", "Skiploom", "Jumpluff", "Aipom", "Sunkern", "Sunflora", "Yanma", "Wooper", "Quagsire", "Espeon", "Umbreon", "Murkrow", "Slowking", "Misdreavus", "Unown", "Wobbuffet", "Girafarig", "Pineco", "Forretress", "Dunsparce", "Gligar", "Steelix", "Snubbull", "Granbull", "Qwilfish", "Scizor", "Shuckle", "Heracross", "Sneasel", "Teddiursa", "Ursaring", "Slugma", "Magcargo", "Swinub", "Piloswine", "Corsola", "Remoraid", "Octillery", "Delibird", "Mantine", "Skarmory", "Houndour", "Houndoom", "Kingdra", "Phanpy", "Donphan", "Porygon2", "Stantler", "Smeargle", "Tyrogue", "Hitmontop", "Smoochum", "Elekid", "Magby", "Miltank", "Blissey", "Raikou", "Entei", "Suicune", "Larvitar", "Pupitar", "Tyranitar", "Lugia", "Ho Oh", "Celebi", "Treecko", "Grovyle", "Sceptile", "Torchic", "Combusken", "Blaziken", "Mudkip", "Marshtomp", "Swampert", "Poochyena", "Mightyena", "Zigzagoon", "Linoone", "Wurmple", "Silcoon", "Beautifly", "Cascoon", "Dustox", "Lotad", "Lombre", "Ludicolo", "Seedot", "Nuzleaf", "Shiftry", "Taillow", "Swellow", "Wingull", "Pelipper", "Ralts", "Kirlia", "Gardevoir", "Surskit", "Masquerain", "Shroomish", "Breloom", "Slakoth", "Vigoroth", "Slaking", "Nincada", "Ninjask", "Shedinja", "Whismur", "Loudred", "Exploud", "Makuhita", "Hariyama", "Azurill", "Nosepass", "Skitty", "Delcatty", "Sableye", "Mawile", "Aron", "Lairon", "Aggron", "Meditite", "Medicham", "Electrike", "Manectric", "Plusle", "Minun", "Volbeat", "Illumise", "Roselia", "Gulpin", "Swalot", "Carvanha", "Sharpedo", "Wailmer", "Wailord", "Numel", "Camerupt", "Torkoal", "Spoink", "Grumpig", "Spinda", "Trapinch", "Vibrava", "Flygon", "Cacnea", "Cacturne", "Swablu", "Altaria", "Zangoose", "Seviper", "Lunatone", "Solrock", "Barboach", "Whiscash", "Corphish", "Crawdaunt", "Baltoy", "Claydol", "Lileep", "Cradily", "Anorith", "Armaldo", "Feebas", "Milotic", "Castform", "Rainy Castform", "Snowy Castform", "Sunny Castform", "Kecleon", "Shuppet", "Banette", "Duskull", "Dusclops", "Tropius", "Chimecho", "Absol", "Wynaut", "Snorunt", "Glalie", "Spheal", "Sealeo", "Walrein", "Clamperl", "Huntail", "Gorebyss", "Relicanth", "Luvdisc", "Bagon", "Shelgon", "Salamence", "Beldum", "Metang", "Metagross", "Regirock", "Regice", "Registeel", "Latias", "Latios", "Kyogre", "Groudon", "Rayquaza", "Jirachi", "Attack Forme Deoxys", "Defense Forme Deoxys", "Normal Forme Deoxys", "Speed Forme Deoxys", "Turtwig", "Grotle", "Torterra", "Chimchar", "Monferno", "Infernape", "Piplup", "Prinplup", "Empoleon", "Starly", "Staravia", "Staraptor", "Bidoof", "Bibarel", "Kricketot", "Kricketune", "Shinx", "Luxio", "Luxray", "Budew", "Roserade", "Cranidos", "Rampardos", "Shieldon", "Bastiodon", "Plant Burmy", "Sandy Burmy", "Trash Burmy", "Plant Wormadam", "Sandy Wormadam", "Trash Wormadam", "Mothim", "Combee", "Vespiquen", "Pachirisu", "Buizel", "Floatzel", "Cherubi", "Overcast Cherrim", "Sunny Cherrim", "East Sea Shellos", "West Sea Shellos", "East Sea Gastrodon", "West Sea Gastrodon", "Ambipom", "Drifloon", "Drifblim", "Buneary", "Lopunny", "Mismagius", "Honchkrow", "Glameow", "Purugly", "Chingling", "Stunky", "Skuntank", "Bronzor", "Bronzong", "Bonsly", "Mime Jr", "Happiny", "Chatot", "Spiritomb", "Gible", "Gabite", "Garchomp", "Munchlax", "Riolu", "Lucario", "Hippopotas", "Hippowdon", "Skorupi", "Drapion", "Croagunk", "Toxicroak", "Carnivine", "Finneon", "Lumineon", "Mantyke", "Snover", "Abomasnow", "Weavile", "Magnezone", "Lickilicky", "Rhyperior", "Tangrowth", "Electivire", "Magmortar", "Togekiss", "Yanmega", "Leafeon", "Glaceon", "Gliscor", "Mamoswine", "Porygon Z", "Gallade", "Probopass", "Dusknoir", "Froslass", "Fan Rotom", "Frost Rotom", "Heat Rotom", "Mow Rotom", "Rotom", "Wash Rotom", "Uxie", "Mesprit", "Azelf", "Dialga", "Palkia", "Heatran", "Regigigas", "Giratina Altered Forme", "Giratina Origin Forme", "Cresselia", "Phione", "Manaphy", "Darkrai", "Land Shaymin", "Sky Shaymin", "Arceus Bug Forme", "Arceus Dark Forme", "Arceus Dragon Forme", "Arceus Electric Forme", "Arceus Fairy Forme", "Arceus Fighting Forme", "Arceus Fire Forme", "Arceus Flying Forme", "Arceus Ghost Forme", "Arceus Grass Forme", "Arceus Ground Forme", "Arceus Ice Forme", "Arceus", "Arceus Poison Forme", "Arceus Psychic Forme", "Arceus Rock Forme", "Arceus Steel Forme", "Arceus Water Forme", "Meltan", "Melmetal"
 
@@ -65,7 +66,7 @@ class Pkm():
         self.maxlvl = max_lvl(name, a=a, d=d, s=s)
         self.maxcp = cp(name, a, d, s, self.maxlvl)
     def __repr__(self):
-        return f'{self.name} {self.maxlvl} ({self.maxcp}): {self.lvl}, {self.a}/{self.d}/{self.s}'
+        return f'{self.name} {self.maxlvl} ({self.maxcp}): L{self.lvl}, {self.a}/{self.d}/{self.s}'
     def ivs(self):
         return (self.a, self.d, self.s)
     def ivs_f(self):
@@ -73,7 +74,7 @@ class Pkm():
     def ivsl_f(self):
         return f'{self.a}/{self.d}/{self.s} {self.lvl}'
     def __lt__(self, other):
-        return True
+        return self.lvl < other.lvl
     def __hash__(self):
         return hash(str(self))
     def export(self):
@@ -164,7 +165,10 @@ def better_than(pkm, a, d, s, l=1):
                     if cp(pkm, a, d, s, lvl/2) <= 1500:
                         if p < product(pkm, a, d, s, lvl/2):
                             l.append((product(pkm, a, d, s, lvl/2), Pkm(pkm, a, d, s, lvl/2)))
-    return '\n'.join(x[1].ivsl_f() for x in sorted(l, reverse=True))
+    return [x[1].ivsl_f() for x in sorted(l, reverse=True)]
+
+def rank(pkm, a, d, s):
+    return len(better_than(pkm, a, d, s)) + 1
 
 def compare(l):
     dic = dict()
@@ -172,8 +176,8 @@ def compare(l):
         name, a, d, s, lvl = x.export()
         lvl = max_lvl(name, a=a, d=d, s=s)
         p = product(name, a, d, s, lvl)
-        dic[x] = p
-    l = sorted(list(dic.items()), key=operator.itemgetter(1), reverse=True)
+        dic[x] = rank(name, a, d, s)
+    l = sorted(list(dic.items()), key=operator.itemgetter(1), reverse=False)
     return l
 
 def comp_txt(name):
@@ -197,8 +201,9 @@ if __name__ == '__main__':
 
     l = [p1,p2,p3,p4,p5]
     # print(compare(l))
-
-    print(cp('Trapinch', 0, 0, 0, 28))
+    # pprint(comp_txt('Infernape'))
+    # print(max_lvl('Infernape'))
+    # print(cp('Trapinch', 0, 0, 0, 28))
 
     # cc = comp_txt(name)
     # print(cc)
@@ -206,7 +211,9 @@ if __name__ == '__main__':
     # print(top[0].percent())
     # print(top)
 
-    # print(better_than(name, 0, 13, 14))
+    name = 'Empoleon'
+    # print(rank(name, 1, 12, 14))
+    print(cp(name, 1, 12, 14, 15))
 
     # print(worst_product(name))
     # print(best_product(name))
